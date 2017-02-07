2bd4a19ab2d487922bd5c86e973725359d76b1d8
Dolan <dolan_miu@hotmail.com>
2017-02-07 10:39:27 +0000

    added phantom js script

--- src/api/linkedin/index.ts
+++ src/api/linkedin/index.ts
@@ -2,25 +2,66 @@ import { Request, Response, Router } from "express";
 import * as passport from "passport";
 import * as request from "request";
 import * as logger from "winston";
-//import { LinkedInPassportWrapper } from "./passport";
+import { LinkedInPassportWrapper } from "../../passport/linkedin";
 import { ProfileFetcher } from "./profile-fetcher";
+var phantom = require("phantom");
+
+const CLIENT_ID = "77jha1j50zl7wa";
+const SECRET = "0fTpTGXQz8LiqHwo";
 
 export class LinkedInRouter {
     public router: Router;
-    //private passportWrapper: LinkedInPassportWrapper;
+    private passportWrapper: LinkedInPassportWrapper;
     private profileFetcher: ProfileFetcher;
 
     constructor(private apiKey: string) {
         this.router = Router();
         this.init();
-        //this.passportWrapper = new LinkedInPassportWrapper();
-        //this.passportWrapper.fetch();
+        this.passportWrapper = new LinkedInPassportWrapper();
+        this.passportWrapper.fetch();
         this.profileFetcher = new ProfileFetcher("77jha1j50zl7wa", "0fTpTGXQz8LiqHwo");
     }
 
     public init(): void {
         this.router.get("/test", (req: Request, res: Response) => {
             this.profileFetcher.connect(res);
+        });
+
+        this.router.get("/phantom", (req: Request, res: Response) => {
+            var _ph, _page, _outObj;
+
+            phantom.create().then(ph => {
+                _ph = ph;
+                return _ph.createPage();
+            }).then(page => {
+                _page = page;
+                // return _page.open(`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost:9000%2Flinkedin%2Fcallback&state=987654321&scope=r_basicprofile`);
+                // return _page.open(`http://localhost:9000/connect/linkedin2`);
+                return _page.open(`http://localhost:9000/linkedin/login`);
+            }).then(status => {
+                console.log(status);
+                return _page.property('content')
+            }).then(content => {
+                console.log(content);
+                _page.evaluate(function () {
+                    window.console.log = function (msg) { alert(msg) };
+
+                    console.log('Clicked');
+                    // find element to send click to
+                    var element = document.getElementsByName('authorize')[0];
+                    console.log(element);
+                    window.console.log(element);
+                    // create a mouse click event
+                    var event = document.createEvent('MouseEvents');
+                    event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+                    // send click to element
+                    element.dispatchEvent(event);
+                    // _page.close();
+                    // _ph.exit();
+                });
+
+            }).catch(e => console.log(e));
         });
 
         this.router.get("/", (req: Request, res: Response) => {
@@ -45,9 +86,10 @@ export class LinkedInRouter {
             // function will not be called.
         });
 
-        this.router.get("/callback", passport.authenticate('linkedin', {
-            successRedirect: '/',
-            failureRedirect: '/'
-        }));
+        this.router.get("/callback", (req: Request, res: Response) => {
+            console.log('authenticated');
+            console.log(req.query.code);
+            res.status(200);
+        });
     }
 }
