5861b918f9ec594ec267345ef486d7f8149695d7
Dolan Miu <dolan_miu@hotmail.com>
2017-02-11 00:13:52 +0000

    cleaned up config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,6 @@ export enum EnvironmentType {
     Development, Production, Test,
 }
 
-export interface IConfig {
-    port: number;
-    apiKey: string;
-}
-
 export class Config implements IConfig {
     public port = process.env.PORT || 9000;
-    public apiKey = process.env.RIOT_API_KEY;
 }
