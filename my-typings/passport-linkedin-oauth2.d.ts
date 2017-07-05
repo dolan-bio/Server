cd1652e21d75433e80bbaad92875bf66cf9bbbd5
Dolan <dolan_miu@hotmail.com>
2017-07-05 02:12:32 +0100

    Typings update

--- my-typings/passport-linkedin-oauth2.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "passport-linkedin-oauth2" {
-    import * as passport from "passport";
-
-    export var Strategy: any;
-
-    export interface LinkedInAuthenticateOptions extends passport.AuthenticateOptions {
-        state?: string;
-    }
-}
---
+++ my-typings/server/error.d.ts
@@ -0,0 +1,3 @@
+declare interface ServerError {
+    message: string;
+}
