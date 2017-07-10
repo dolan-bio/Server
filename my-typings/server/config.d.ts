39901f9086f29ac06157585236503a83cc21d709
Dolan Miu <dolan_miu@hotmail.com>
2017-07-11 00:35:28 +0100

    Removed linkedin from config

--- my-typings/server/config.d.ts
+++ my-typings/server/config.d.ts
@@ -7,12 +7,6 @@ declare interface IConfig {
     port: string | number;
     baseUrl: string;
     mongoUri: string;
-    linkedIn: {
-        clientId: string,
-        secret: string,
-        email: string,
-        password: string,
-    };
     github: {
         token: string,
     };
--- src/config/development-config.ts
+++ src/config/development-config.ts
@@ -2,12 +2,6 @@ export const DevelopmentConfig: IConfig = {
     port: process.env.PORT || 9000,
     baseUrl: `http://localhost:${this.port}`,
     mongoUri: process.env.MONGODB_URI,
-    linkedIn: {
-        clientId: process.env.LINKED_IN_CLIENT_ID,
-        secret: process.env.LINKED_IN_SECRET,
-        email: process.env.LINKED_IN_EMAIL,
-        password: process.env.LINKED_IN_PASSWORD,
-    },
     github: {
         token: process.env.GITHUB_TOKEN,
     },
