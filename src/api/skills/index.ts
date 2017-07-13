211a4a29cf1fabf9e45db3fee5e45da7c5d74b48
Dolan <dolan_miu@hotmail.com>
2017-07-13 19:32:00 +0100

    Made base64 part of the response

--- src/api/skills/index.ts
+++ src/api/skills/index.ts
@@ -5,11 +5,10 @@ import * as logger from "winston";
 import { Observable } from "rxjs/Rx";
 import { ImageFetcher } from "./image-fetcher";
 import { SkillsFetcher } from "./skill-fetcher";
-import { ISkillDocument } from "./skill-model";
 
 interface ISkillCombinedResult {
-    image: GoogleImageResult;
-    document: ISkillDocument;
+    image: string;
+    name: string;
 }
 
 export class SkillsRouter {
@@ -26,10 +25,21 @@ export class SkillsRouter {
         this.combinedObservable = this.skillsFetcher.WhenFetchedSkills.flatMap((skills) => {
             const observables: Observable<ISkillCombinedResult>[] = [];
             skills.forEach((skill) => {
-                observables.push(this.imageFetcher.findImage(skill.name).map((image) => {
+                observables.push(this.imageFetcher.findImage(skill.name).flatMap((image) => {
+                    const getAsObservable = Observable.bindCallback<[Error, request.RequestResponse, Buffer]>(request.get);
+                    return getAsObservable({
+                        url: image.url,
+                        encoding: null,
+                    }).map(([err, response, body]) => {
+                        const type = response.headers["content-type"];
+                        const prefix = "data:" + type + ";base64,";
+                        const base64 = body.toString("base64");
+                        return prefix + base64;
+                    });
+                }).map((base64) => {
                     return {
-                        image: image,
-                        document: skill,
+                        image: base64,
+                        name: skill.name,
                     };
                 }));
             });
@@ -51,29 +61,6 @@ export class SkillsRouter {
                     message: "Something went wrong with the server",
                 };
                 res.status(500).send(errorResponse);
-            });
-        });
-
-        this.router.get("/:id/image", (req: Request, res: Response) => {
-            const skillId = req.params.id as string;
-
-            this.skillsFetcher.WhenFetchedSkills.map((skills) => {
-                return skills.find((skill) => skill._id.toString() === skillId).name;
-            }).flatMap((skillName) => {
-                return this.imageFetcher.findImage(skillName);
-            }).flatMap((image) => {
-                const getAsObservable = Observable.bindCallback<[Error, request.RequestResponse, Buffer]>(request.get);
-                return getAsObservable({
-                    url: image.url,
-                    encoding: null,
-                }).map(([err, response, body]) => {
-                    const type = response.headers["content-type"];
-                    const prefix = "data:" + type + ";base64,";
-                    const base64 = body.toString("base64");
-                    return prefix + base64;
-                });
-            }).subscribe((base64) => {
-                res.status(200).send(base64);
             });
         });
     }
